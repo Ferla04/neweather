ba4706fce6f9d85a6d13d4f9548fe27ae8f3413a
Ferla04 <fer.velas135@gmail.com>
2023-05-11 17:34:44 -0500

    Lenguage changed

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -5,7 +5,7 @@ import './Social.css'
 export function Social () {
   return (
     <aside>
-      <div className='contact'><h3>CONTACT</h3></div>
+      <div className='contact'><h3>CONTACTO</h3></div>
       <section className='contactItems'>
         {
           DEVS.map(dev => (
